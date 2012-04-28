9141441e32db37382415d8e469dbc2e2d744e5a0
eaut <eaut@boehm.org>
2012-04-28 18:11:11 +0200

    now the real file

--- cm.mk
+++ cm.mk
@@ -1,1 +1,22 @@
-../p1/cm_p1n.mk
+#
+# CyanogenMod product configuration
+#
+
+# --------------------------------------------------------------------------------
+# Inherit CM stuff
+# --------------------------------------------------------------------------------
+
+$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# --------------------------------------------------------------------------------
+# Inherit device configuration
+# --------------------------------------------------------------------------------
+
+$(call inherit-product, device/samsung/p1/full_p1n.mk)
+
+# --------------------------------------------------------------------------------
+# override product name - the rest is already defined in full_p1.mk
+# --------------------------------------------------------------------------------
+
+PRODUCT_NAME := cm_p1n
