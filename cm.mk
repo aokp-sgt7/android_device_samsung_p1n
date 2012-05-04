deb016b3519ab50101e3c1547873b2506cfa8957
eaut <eaut@boehm.org>
2012-05-04 23:52:17 +0200

    adjust to makefile simplification
    Change-Id: I0dcc5bd2771a23d07ff2bf18b286d35a4b625ec2

--- cm.mk
+++ cm.mk
@@ -13,10 +13,19 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 # --------------------------------------------------------------------------------
 
-$(call inherit-product, device/samsung/p1/full_p1n.mk)
+$(call inherit-product, device/samsung/p1/p1.mk)
 
 # --------------------------------------------------------------------------------
-# override product name - the rest is already defined in full_p1n.mk
+# product specific settings
 # --------------------------------------------------------------------------------
 
 PRODUCT_NAME := cm_p1n
+PRODUCT_DEVICE := p1n
+PRODUCT_MODEL := GT-P1000N
+
+# kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/p1
+TARGET_KERNEL_CONFIG := cyanogenmod_$(PRODUCT_DEVICE)_defconfig
+
+# asserts
+TARGET_OTA_ASSERT_DEVICE := galaxytab,$(PRODUCT_MODEL)
