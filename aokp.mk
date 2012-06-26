a67092608ce79800493c59be0e50d51c8683018d
stimpz0r <stimpz0r@kriticalcrew.org>
2012-06-26 23:47:21 +1000

    still attempting to fix p1n builds... (fail on me)

--- aokp.mk
+++ aokp.mk
@@ -1,6 +1,6 @@
-$(call inherit-product, vendor/cm/config/gsm.mk)
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/p1/full_p1n.mk)
+$(call inherit-product, vendor/aokp/configs/common_tablet.mk)
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
 
 PRODUCT_NAME := full_p1n
 PRODUCT_DEVICE := p1n
