a468dec966de6255e0df02a5c8b9078b05d830fc
stimpz0r <stimpz0r@kriticalcrew.org>
2012-08-22 00:26:00 +1000

    fix builds for all p1 variants - move kernel and assert lines to BoardConfig.mk
    - thanks to @henryedwardrose for the help with this one! :)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,1 +1,8 @@
 include device/samsung/p1/BoardConfig.mk
+
+# kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/p1
+TARGET_KERNEL_CONFIG := cyanogenmod_p1n_defconfig
+
+# asserts
+TARGET_OTA_ASSERT_DEVICE := galaxytab,GT-P1000N
--- aokp.mk
+++ aokp.mk
@@ -5,10 +5,3 @@ $(call inherit-product, vendor/aokp/configs/gsm.mk)
 PRODUCT_NAME := full_p1n
 PRODUCT_DEVICE := p1n
 PRODUCT_MODEL := GT-P1000N
-
-# kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/p1
-TARGET_KERNEL_CONFIG := cyanogenmod_$(PRODUCT_DEVICE)_defconfig
-
-# asserts
-TARGET_OTA_ASSERT_DEVICE := galaxytab,$(PRODUCT_MODEL)
