3835a95bf4350d2fd7e3943e6da6e3dca5711a73
stimpz0r <stimpz0r@kritical.co>
2012-09-23 12:36:58 +1000

    still not working :(

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,8 +1,11 @@
-include device/samsung/p1/BoardConfig.mk
+include device/samsung/p1-common/BoardConfigCommon.mk
+
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/p1/releasetools/p1_ota_from_target_files
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/p1/releasetools/p1_img_from_target_files
 
 # kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/p1
 TARGET_KERNEL_CONFIG := cyanogenmod_p1n_defconfig
 
 # asserts
-TARGET_OTA_ASSERT_DEVICE := galaxytab,GT-P1000N
+TARGET_OTA_ASSERT_DEVICE := galaxytab,GT-P1000N,p1n
--- aokp.mk
+++ aokp.mk
@@ -1,7 +1,16 @@
-$(call inherit-product, device/samsung/p1/full_p1n.mk)
+$(call inherit-product, device/samsung/p1n/full_p1n.mk)
 $(call inherit-product, vendor/aokp/configs/common_tablet.mk)
 $(call inherit-product, vendor/aokp/configs/gsm.mk)
 
+# Release name
+PRODUCT_RELEASE_NAME := P1N
+
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := full_p1n
 PRODUCT_DEVICE := p1n
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-P1000N
+
+# Build.prop overrides for Market Compability
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-P1000 TARGET_DEVICE=GT-P1000 BUILD_ID=GINGERBREAD BUILD_FINGERPRINT=samsung/GT-P1000/GT-P1000:2.3.5/GINGERBREAD/XXJVT:user/release-keys PRIVATE_BUILD_DESC="GT-P1000-user 2.3.5 GINGERBREAD XXJVT release-keys"
